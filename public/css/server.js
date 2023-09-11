69106b348d6fd68ecc5cba534ecd8c26b98214d4
Victor Nieves <c_cc_vic@yahoo.com>
2023-09-11 18:18:56 -0400

    updated server js code

--- public/css/server.js
+++ public/css/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded ({extended:true}));
 app.use(express.static(path.join(__dirname, "public")));
 
+ 
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
