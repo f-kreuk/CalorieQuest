65bbfcef4d2d204ed92be74415c313501a6922dd
Florian Kreuk <fkreuk@cartermario.com>
2023-09-12 13:56:30 -0400

    updated seeds file and updated index.js file from models

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const app = express();
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {
@@ -25,8 +27,6 @@ const sess = {
 };
 
 app.use(session(sess));
-
-const app = express();
 
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
