554300b6ab718025562fd781b965dd3bae57c2fe
Florian Kreuk <fkreuk@cartermario.com>
2023-09-17 12:16:05 -0400

    still debugging the routes, there is a data validation error between form and model

--- models/Quest.js
+++ models/Quest.js
@@ -16,63 +16,66 @@ Quest.init(
       allowNull: false,
     },
     age: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.FLOAT,
       allowNull: true,
     },
-    heightFt: {
-      type: DataTypes.INTEGER,
+    height_ft: {
+      type: DataTypes.FLOAT,
       allowNull: true,
     },
-    heightIn: {
-      type: DataTypes.INTEGER,
+    height_in: {
+      type: DataTypes.FLOAT,
       allowNull: true,
     },
-    heightCentimeters: {
-      type: DataTypes.INTEGER,
+    height_centimeters: {
+      type: DataTypes.FLOAT,
       allowNull: true,
     },
-    startingDate: {
+    starting_date: {
       type: DataTypes.DATE,
       allowNull: true,
     },
-    startingWeight: {
-      type: DataTypes.INTEGER,
+    starting_weight: {
+      type: DataTypes.FLOAT,
       allowNull: true,
     },
-    goalWeight: {
-      type: DataTypes.INTEGER,
+    goal_weight: {
+      type: DataTypes.FLOAT,
       allowNull: true,
     },
-    dailyLossGoal: {
-        type: DataTypes.INTEGER,
+    daily_loss_goal: {
+        type: DataTypes.FLOAT,
         allowNull: true,
     },
-    dailyLossActual: {
-        type: DataTypes.INTEGER,
+    daily_loss_actual: {
+        type: DataTypes.FLOAT,
         allowNull: true,
     },
-    actualToGoal: {
-        type: DataTypes.INTEGER,
+    actual_to_goal: {
+        type: DataTypes.FLOAT,
         allowNull: true,
     },
-    startingWeightKg: {
-        type: DataTypes.INTEGER,
+    starting_weight_kg: {
+        type: DataTypes.FLOAT,
         allowNull: true,
     },
     progress: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.FLOAT,
         allowNull: true,
     },
-    expectedFinalWeight: {
-        type: DataTypes.INTEGER,
+    expected_final_weight: {
+        type: DataTypes.FLOAT,
         allowNull: true,
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     user_id: {
         type: DataTypes.INTEGER,
         references: {
             model: 'user',
             key: 'id',
-            unique: false,
         },
     },
     active: {
@@ -82,6 +85,7 @@ Quest.init(
   },
   {
     sequelize,
+    timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'quest',
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,33 +1,41 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const heightFt = document.querySelector('#heightFt-profile').value.trim();
-  const heightIn = document.querySelector('#heightIn-profile').value.trim();
+  const height_ft = document.querySelector('#heightFt-profile').value.trim();
+  const height_in = document.querySelector('#heightIn-profile').value.trim();
   const gender = document.querySelector('#gender-profile').value.trim();
   const age = document.querySelector('#age-profile').value.trim();
-  const startingDate = document.querySelector('#startingDate-profile').value.trim();
-  const startingWeight = document.querySelector('#startingWeight-profile').value.trim();
-  const goalWeight = document.querySelector('#goalWeight-profile').value.trim();
+  const starting_date = document.querySelector('#startingDate-profile').value.trim();
+  const starting_weight = document.querySelector('#startingWeight-profile').value.trim();
+  const goal_weight = document.querySelector('#goalWeight-profile').value.trim();
   
 
 
-  if (heightFt && heightIn && gender && age && startingDate && startingWeight && goalWeight) {
-    const response = await fetch(`/api/quest`, {
+  if (height_ft && height_in && gender && age && starting_date && starting_weight && goal_weight) {
+    const response = await fetch(`/api/quests`, {
       method: 'POST',
-      body: JSON.stringify({heightFt , heightIn, gender , age ,  startingDate, startingWeight, goalWeight}),
+      body: JSON.stringify({ height_ft, height_in, gender, age, starting_date, starting_weight, goal_weight }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/quest');
+      console.log("Works");
+      document.location.replace('/profile');
     } else {
-      alert('Failed to create new quest.');
+      console.log("didn't work");
+      if (response.status === 400) {
+        alert('Validation error: Please check your inputs.');
+      } else {
+        alert('Failed to create new quest.');
+      }
     }
   }
 };
 
-document
-  .querySelector('.profile-form')
-  .addEventListener('submit', newFormHandler);
+document.addEventListener('DOMContentLoaded', () => {
+  document
+    .querySelector('.new-quest-form')
+    .addEventListener('submit', newFormHandler);
+});
